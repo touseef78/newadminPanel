347d147bfca199c406c3358c9ed50546c1d3ed10
touseef78 <marajzm786@gmail.com>
2023-08-03 17:19:16 +0500

    updare

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://192.168.100.17:8001/api/"; // change this if you want to use a different url for APIs
+axios.defaults.baseURL = "http://192.168.100.20:8001/api/"; // change this if you want to use a different url for APIs
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
