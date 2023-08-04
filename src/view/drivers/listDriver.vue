4377ffef90c1ee979d87fd594b4f174ed8332032
awais773 <awaissafdar773@gmail.com>
2023-08-04 19:46:07 +0500

    update

--- src/view/drivers/listDriver.vue
+++ src/view/drivers/listDriver.vue
@@ -89,19 +89,27 @@
               />
             </svg>
 
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              style="color: orange; margin-left: 10px; margin-bottom: 10px"
-              class="bi bi-pencil"
-              viewBox="0 0 16 16"
-            >
-              <path
+            <b-button
+      @click="editUser(row.item.id)"
+      variant="link"
+      class="p-0"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="currentColor"
+        style="color: orange; margin-left: 10px; margin-bottom: 10px"
+        class="bi bi-pencil"
+        viewBox="0 0 16 16"
+      >
+        <!-- ... your existing SVG path ... -->
+        <path
                 d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
               />
-            </svg>
+      </svg>
+    </b-button>
+            
 
             <!-- ... your existing action button code ... -->
             <b-button @click="updateStatus(row.item)" :variant="row.item.status === 'Approved' ? 'warning' : 'primary'" class="mb-8 mr-8">
@@ -270,6 +278,11 @@ export default {
         console.error('Error updating status:', error);
       });
   },
+
+  editUser(userId) {
+      // Use the $router.push method to navigate to the edit page
+      this.$router.push({ name: 'editDrivers', params: { id: userId } });
+    },
     
   },
 };
