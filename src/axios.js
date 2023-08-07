111e29a6c557e5376939832d292db5ec07d94c16
touseef78 <marajzm786@gmail.com>
2023-08-07 17:47:15 +0500

    update now

--- src/axios.js
+++ src/axios.js
@@ -2,3 +2,8 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://127.0.0.1:8000/api/"; // change this if you want to use a different url for APIs
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+
+
+
+
+
