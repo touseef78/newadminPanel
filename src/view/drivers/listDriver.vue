c63e9fbe15b1b47e68bdb16cd9931e582ba6dd0b
touseef78 <marajzm786@gmail.com>
2023-08-07 16:31:15 +0500

    updatw

--- src/view/drivers/listDriver.vue
+++ src/view/drivers/listDriver.vue
@@ -23,91 +23,54 @@
     <!-- filter end -->
     <b-row>
       <div class="col-12 mt-16">
-        <b-table id="dataTable" :items="users" :fields="fields" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc"
-          responsive>
+        <b-table id="dataTable" :items="users" :fields="fields" :current-page="currentPage" :per-page="perPage"
+          :filter="filter" :filter-included-fields="filterOn" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection" stacked="md" show-empty @filtered="onFiltered" y responsive>
           <!-- Action Button Code -->
           <template #cell(actions)="row">
-<b-button  @click="showDrivers(row.item.id)" variant="link" class="p-0" >
-  <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              style="
+            <b-button @click="showDrivers(row.item.id)" variant="link" class="p-0">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" style="
                 color: rgba(0, 255, 195, 0.87);
                 margin-left: 6px;
                 margin-bottom: 10px;
               " class="bi bi-eye" viewBox="0 0 16 16">
-              <path
-                d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-              <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-            </svg>
-</b-button>
-
-            <b-button
-              @click="showDeleteConfirmation = true"
-              variant="link"
-              class="p-0"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                fill="currentColor"
-                style="
-                  color: rgba(0, 255, 195, 0.87);
-                  margin-left: 6px;
-                  margin-bottom: 10px;
-                "
-                class="bi bi-eye"
-                viewBox="0 0 16 16"
-              >
-                <!-- ... your SVG path ... -->
                 <path
-                  d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"
-                />
+                  d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
               </svg>
             </b-button>
 
-            <b-modal
-              v-model="showDeleteConfirmation"
-              title="Delete Confirmation"
-            >
+            <b-button @click="showDeleteConfirmation = true" variant="link" class="p-0">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" style="
+                  color: rgba(0, 255, 195, 0.87);
+                  margin-left: 6px;
+                  margin-bottom: 10px;
+                " class="bi bi-eye" viewBox="0 0 16 16">
+                <!-- ... your SVG path ... -->
+                <path
+                  d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+              </svg>
+            </b-button>
+
+            <b-modal v-model="showDeleteConfirmation" title="Delete Confirmation">
               <p>Are you sure you want to delete this item?</p>
               <template #modal-footer>
-                <b-button variant="danger" @click="deleteItem(row.item.id)"
-                  >Delete</b-button
-                >
-                <b-button
-                  variant="secondary"
-                  @click="showDeleteConfirmation = false"
-                  >Cancel</b-button
-                >
+                <b-button variant="danger" @click="deleteItem(row.item.id)">Delete</b-button>
+                <b-button variant="secondary" @click="showDeleteConfirmation = false">Cancel</b-button>
               </template>
             </b-modal>
             <b-button @click="editUser(row.item.id)" variant="link" class="p-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                fill="currentColor"
-                style="color: orange; margin-left: 10px; margin-bottom: 10px"
-                class="bi bi-pencil"
-                viewBox="0 0 16 16"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
+                style="color: orange; margin-left: 10px; margin-bottom: 10px" class="bi bi-pencil" viewBox="0 0 16 16">
                 <!-- ... your existing SVG path ... -->
                 <path
-                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
-                />
+                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
               </svg>
             </b-button>
 
             <!-- ... your existing action button code ... -->
-            <b-button
-              @click="updateStatus(row.item)"
-              :variant="row.item.status === 'Approved' ? 'warning' : 'primary'"
-              class="mb-8 mr-8"
-            >
+            <b-button @click="updateStatus(row.item)" :variant="row.item.status === 'Approved' ? 'warning' : 'primary'"
+              class="mb-8 mr-8">
               {{ row.item.status === "Approved" ? "Pending" : "Approved" }}
             </b-button>
           </template>
@@ -123,16 +86,42 @@
             </b-input-group>
           </b-form-group>
         </b-table>
+        <div class="mx-8 d-flex justify-content-end">
+          <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage"
+            aria-controls="my-table"></b-pagination>
+        </div>
         <b-row class="mt-16 align-items-center justify-content-end">
-          <b-col class="hp-flex-none w-auto">
+          <!-- <b-col class="hp-flex-none w-auto">
             <b-form-group label-for="per-page-select" label-size="sm" class="mb-0">
               <b-form-select id="per-page-select" v-model="perPage" :options="pageOptions"></b-form-select>
             </b-form-group>
-          </b-col>
+          </b-col> -->
 
-          <b-col class="hp-flex-none w-auto">
+          <!-- <b-col class="hp-flex-none w-auto">
             <b-pagination align="end" v-model="currentPage" :total-rows="totalRows" :per-page="perPage"></b-pagination>
-          </b-col>
+          </b-col> -->
+          <b-row>
+            <!-- <div class="col-12 mt-16">
+              <div class="overflow-auto">
+                <p class="mt-3 hp-p1-body">Current Page: {{ currentPage }}</p>
+
+                <b-table id="my-table" :items="users" :per-page="perPage" :current-page="currentPage" small></b-table>
+              </div>
+
+              <div class="mx-8 d-flex justify-content-end">
+                <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage"
+                  aria-controls="my-table"></b-pagination>
+              </div>
+            </div> -->
+
+            <div v-if="codeActive" class="col-12 mt-24 hljs-container" :class="{ active: codeActiveClass }">
+              <pre v-highlightjs>
+          <code class="hljs html">
+            {{ codeText }}
+          </code>
+        </pre>
+            </div>
+          </b-row>
         </b-row>
       </div>
     </b-row>
@@ -155,17 +144,21 @@ import {
 } from "bootstrap-vue";
 import axios from "axios";
 import Papa from "papaparse";
+
 // new code 
 // import code from "./code";   
 // new code end
 export default {
   data() {
     return {
+      perPage: 10,
+      currentPage: 1,
       sortBy: "age",
       sortDesc: false,
-      users: [], // Instead of 'items', use 'users' array to store fetched data
+      users: [
+      ], // Instead of 'items', use 'users' array to store fetched data
       fields: [
-        { key: "id", sortable: true },
+        // { key: "id", sortable: true },
         { key: "name", sortable: true },
         { key: "mobile", sortable: true },
         { key: "email", sortable: true },
@@ -179,10 +172,10 @@ export default {
       ],
 
       filter: "", // Define filter property for search functionality
-      perPage: 5, // Set a default number of rows per page
       totalRows: 0, // Initialize totalRows to 0
       showDeleteConfirmation: false,
       itemIdToDelete: null,
+
     };
   },
   components: {
@@ -206,9 +199,13 @@ export default {
           return { text: f.label, value: f.key };
         });
     },
+    rows() {
+      return this.users.length;
+    },
   },
   mounted() {
     this.fetchData();
+
   },
   methods: {
     fetchData() {
@@ -216,7 +213,7 @@ export default {
         .get("drivers") // Replace 'your_api_endpoint_url_here' with your actual API URL
         .then((response) => {
           this.users = response.data.data;
-          this.totalRows = this.users.length; // Update the totalRows based on the fetched data
+          this.totalRows = this.users.length;
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -241,17 +238,7 @@ export default {
       URL.revokeObjectURL(url);
     },
 
-    updateStatus(user) {
-      user.status = user.status === "Approved" ? "Pending" : "Approved";
-      axios
-        .put(`drivers/${user.id}`, user)
-        .then((response) => {
-          console.log("Status updated successfully:", response.data);
-        })
-        .catch((error) => {
-          console.error("Error updating status:", error);
-        });
-    },
+
 
     editUser(userId) {
       this.$router.push({ name: "editDrivers", params: { id: userId } });
