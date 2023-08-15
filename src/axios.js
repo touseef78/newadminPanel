fd4c6418078c3b719ab660095d89cc327151f8ac
touseef78 <marajzm786@gmail.com>
2023-08-15 13:31:03 +0500

    update

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://boltapi.fastnetstaffing.in/api/"; // change this if you want to use a different url for APIs
+axios.defaults.baseURL = "http://127.0.0.1:8000/api/"; // change this if you want to use a different url for APIs
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 
