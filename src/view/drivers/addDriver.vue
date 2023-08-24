2b334a9af3569a378cd1bf4969d4d43bf79a7f8a
touseef78 <marajzm786@gmail.com>
2023-08-24 18:03:40 +0500

    update

--- src/view/drivers/addDriver.vue
+++ src/view/drivers/addDriver.vue
@@ -127,13 +127,6 @@
                   </b-form-input>
                 </b-form-group>
               </div>
-              <div class="col-md-4 col-12">
-                <b-form-group id="input-group-2" label="Profile Picture:" label-for="profile_picture">
-                  <div style="margin-left: 3px; margin-bottom: 15px">
-                    <input type="file" accept="image/*" id="profile_picture" @change="onProfilePictureChange" />
-                  </div>
-                </b-form-group>
-              </div>
 
               <div v-if="selectedCarType === 'Company'" class="col-md-4 col-12">
                 <b-form-group id="input-group-2" label="Select Car:" label-for="vehicle_id">
@@ -146,7 +139,7 @@
                 </b-form-group>
               </div>
               <!-- Dropdown  of Add Vehicle -->
-              <div v-if="selectedCarType === 'Own'">
+              <div v-if="selectedCarType === 'Own'" style="padding-left: 15px;">
                 <b-row>
                   <div class="col-12 mt-16">
                     <div style="
@@ -218,6 +211,13 @@
                                                 </pre>
                   </div>
                 </b-row>
+              </div>
+              <div class="col-md-4 col-12">
+                <b-form-group id="input-group-2" label="Profile Picture:" label-for="profile_picture">
+                  <div style="margin-left: 3px; margin-bottom: 15px">
+                    <input type="file" accept="image/*" id="profile_picture" @change="onProfilePictureChange" />
+                  </div>
+                </b-form-group>
               </div>
             </div>
 
