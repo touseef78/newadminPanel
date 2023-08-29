2a3fcae5eaabd4032190f5fba9b848b62ea664c1
awais773 <awaissafdar773@gmail.com>
2023-08-29 13:54:23 +0500

    update

---
+++ src/view/report/importFile.vue
@@ -0,0 +1,75 @@
+
+<template>
+    <b-card>
+        <!-- filter  -->
+        <div class="col-12 mt-16">
+            <div>
+                <b-row class="align-items-center">
+                    <b-col lg="6" class="my-1 d-flex justify-content-end">
+                        <b-button type="submit" variant="primary" class="mb-8 mr-8">Import File</b-button>
+                    </b-col>
+                </b-row>
+            </div>
+        </div>
+        <!-- filter end -->
+    </b-card>
+</template>
+
+<script>
+import {
+    BRow,
+    BCol,
+    BCard,
+    BButton,
+    BTable,
+    BFormGroup,
+    BInputGroup,
+    BFormInput,
+    BFormSelect,
+    BPagination,
+    BInputGroupAppend,
+    BSpinner,
+} from "bootstrap-vue";
+import axios from "axios";
+import Papa from "papaparse";
+
+// new code 
+// import code from "./code";   
+// new code end
+export default {
+    data() {
+        return {
+            perPage: 8,
+            currentPage: 1,
+            sortBy: "age",
+            sortDesc: false,
+            filter: "", // Define filter property for search functionality
+            totalRows: 0, // Initialize totalRows to 0
+            showDeleteConfirmation: false,
+            itemIdToDelete: null,
+            loading: false,
+
+        };
+    },
+    components: {
+        BRow,
+        BCol,
+        BCard,
+        BButton,
+        BTable,
+        BFormGroup,
+        BInputGroup,
+        BFormInput,
+        BFormSelect,
+        BPagination,
+        BInputGroupAppend,
+        BSpinner,
+
+    },
+
+    methods: {
+      
+
+    },
+};
+</script>
