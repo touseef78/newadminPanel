b85e54b340341ac2dcd355cc7bd8754718dd8f98
awais773 <awaissafdar773@gmail.com>
2023-10-19 18:40:06 +0500

    update

--- src/view/main/dashboards/analytics/index.vue
+++ src/view/main/dashboards/analytics/index.vue
@@ -1,147 +1,312 @@
 <template>
     <b-row class="mb-32">
-        <!-- <b-col cols="12" class="mb-32">
-            <h3>Welcome back, Edward 👋</h3>
-            <p class="hp-p1-body mb-0">Your current status and analytics are here</p>
-        </b-col> -->
-
-        <b-col cols="12">
+      <!-- <b-col cols="12" class="mb-32">
+              <h3>Welcome back, Edward 👋</h3>
+              <p class="hp-p1-body mb-0">Your current status and analytics are here</p>
+          </b-col> -->
+  
+      <b-col cols="12">
+        <b-row>
+          <b-col class="flex-grow-1 overflow-hidden">
             <b-row>
-                <b-col class="flex-grow-1 overflow-hidden">
-                    <b-row>
-                        <b-col cols="12" class="mb-32">
-
-                        </b-col>
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <customer-feature-card />
-                        </b-col>
-
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <orders-feature-card />
-                        </b-col>
-
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <sale-feature-card />
-                        </b-col>
-
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <profit-feature-card />
-                        </b-col>
-
-
-                        <!-- new code  -->
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <vehicle-feature-card />
-                        </b-col>
-
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <earning-feature-card />
-                        </b-col>
-
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <total-profit-card />
-                        </b-col>
-
-                        <b-col cols="12" md="6" xl="3" class="mb-32">
-                            <total-expense-card />
-                        </b-col>
-                    </b-row>
-                </b-col>
-
-
-            </b-row>
-            <b-row>
-                <!-- new code  -->
-                 <b-col cols="12" xl="8">
+              <b-col cols="12" class="mb-32"> </b-col>
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
                   <b-row>
-                    <b-col cols="12" class="mb-32">
-                      <new-revenue-colum />
+                    <b-col class="hp-flex-none w-auto">
+                      <b-avatar
+                        class="bg-primary-4 hp-bg-color-dark-primary"
+                        size="48px"
+                      >
+                        <i
+                          class="iconly-Light-People text-primary hp-text-color-dark-primary-2"
+                          style="font-size: 24px"
+                        ></i>
+                      </b-avatar>
                     </b-col>
-        
-                  </b-row>
-              
-                </b-col>
-                <b-col cols="12" xl="4">
-                  <b-row>
-                    <b-col cols="12" class="mb-32">
-                      <new-earning-card />
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                          {{ driverCount }}<span class="hp-badge-text ml-8 text-primary"></span>
+                      </h3>
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        Total Driver
+                      </p>
                     </b-col>
                   </b-row>
-                </b-col>
-            </b-row>
-            
-            
-             
-
+                </b-card>
+              </b-col>
+  
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                    <b-col class="hp-flex-none w-auto">
+                      <b-avatar
+                        class="bg-secondary-4 hp-bg-color-dark-secondary"
+                        size="48px"
+                      >
+                        <i
+                          class="iconly-Light-Buy text-secondary"
+                          style="font-size: 24px"
+                        ></i>
+                      </b-avatar>
+                    </b-col>
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        {{ vehicleCount }}<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        Total Vehicle
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+  
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                    <b-col class="hp-flex-none w-auto">
+                      <b-avatar
+                        class="bg-secondary-4 hp-bg-color-dark-secondary"
+                        size="48px"
+                      >
+                        <i
+                          class="iconly-Light-Ticket text-warning"
+                          style="font-size: 24px"
+                        ></i>
+                      </b-avatar>
+                    </b-col>
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        {{ b2bCount }}<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        B2B
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+  
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                    <b-col class="hp-flex-none w-auto">
+          <b-avatar class="bg-danger-4 hp-bg-color-dark-danger" size="48px">
+            <i
+                class="iconly-Light-Discount text-danger"
+                style="font-size: 24px"
+              ></i>
+          </b-avatar>
         </b-col>
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        {{ b2bDriverCount }}<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        B2B Driver
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+  
+              <!-- new code  -->
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                    <b-col class="hp-flex-none w-auto">
+                  <b-avatar class="bg-primary-4 hp-bg-color-dark-primary" size="48px">
+                      <i class="iconly-Light-People text-primary hp-text-color-dark-primary-2" style="font-size: 24px"></i>
+                  </b-avatar>
+              </b-col>
+  
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        {{ b2bVehicleCount }}<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        B2B Vehicle
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+  
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                    <b-col class="hp-flex-none w-auto">
+                  <b-avatar class="bg-primary-4 hp-bg-color-dark-primary" size="48px">
+                       <i
+                  class="iconly-Light-Buy text-secondary"
+                  style="font-size: 24px"
+                ></i>
+                  </b-avatar>
+              </b-col>
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        {{ totalEarning }}<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        Total Earning
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+  
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                    <b-col class="hp-flex-none w-auto">
+                  <b-avatar class="bg-primary-4 hp-bg-color-dark-primary" size="48px">
+                       <i
+                  class="iconly-Light-Ticket text-warning"
+                  style="font-size: 24px"
+                ></i>
+                  </b-avatar>
+              </b-col>
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        242<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        Total Profit
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+  
+              <b-col cols="12" md="6" xl="3" class="mb-32">
+                <b-card>
+                  <b-row>
+                   <b-col class="hp-flex-none w-auto">
+                  <b-avatar class="bg-primary-4 hp-bg-color-dark-primary" size="48px">
+                  
+                      <i
+                    class="iconly-Light-Discount text-danger"
+                    style="font-size: 24px"
+                  ></i>
+                  </b-avatar>
+              </b-col>
+  
+                    <b-col class="pl-0">
+                      <h3 class="mb-4 mt-8">
+                        {{ totalExpense }}<span class="hp-badge-text ml-8 text-secondary"></span>
+                      </h3>
+  
+                      <p
+                        class="hp-p1-body mb-0 text-black-80 hp-text-color-dark-30"
+                      >
+                        Total Expense
+                      </p>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+        <b-row>
+          <!-- new code  -->
+          <b-col cols="12" xl="8">
+            <b-row>
+              <b-col cols="12" class="mb-32">
+                <new-revenue-colum />
+              </b-col>
+            </b-row>
+          </b-col>
+          <b-col cols="12" xl="4">
+            <b-row>
+              <b-col cols="12" class="mb-32">
+                <new-earning-card />
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-col>
     </b-row>
-</template>
-
-<script>
-import { BRow, BCol } from "bootstrap-vue";
-import CustomerFeatureCard from "../../widgets/cards/advance/CustomerFeatureCard.vue";
-import OrdersFeatureCard from "../../widgets/cards/advance/OrdersFeatureCard.vue";
-import SaleFeatureCard from "../../widgets/cards/advance/SaleFeatureCard.vue";
-import ProfitFeatureCard from "../../widgets/cards/advance/ProfitFeatureCard.vue";
-import UpgradePlanCardOneBg from "../../widgets/cards/advance/UpgradePlanCardOneBg.vue";
-import VisitersCard from "../../widgets/cards/analytics/VisitersCard.vue";
-import OrderVerticalCard from "../../widgets/cards/statistics/OrderVerticalCard.vue";
-import UsersVerticalCard from "../../widgets/cards/statistics/UsersVerticalCard.vue";
-import SubscribeVerticalCard from "../../widgets/cards/statistics/SubscribeVerticalCard.vue";
-import TicketVerticalCard from "../../widgets/cards/statistics/TicketVerticalCard.vue";
-import BestTeamCard from "../../widgets/cards/advance/BestTeamCard.vue";
-import EarningsCard from "../../widgets/cards/advance/EarningsCard.vue";
-import ProjectTableCard from "./ProjectTableCard.vue";
-import ExpensesCard from "../../widgets/cards/advance/ExpensesCard.vue";
-import DownloadCard from "../../widgets/cards/advance/DownloadCard.vue";
-import RevenueRadarCard from '../../widgets/cards/analytics/RevenueRadarCard.vue';
-//  new code 
-import VehicleFeatureCard from "../../widgets/cards/advance/VehicleFeatureCard.vue";
-import EarningFeatureCard from "../../widgets/cards/advance/EarningFeatureCard.vue";
-import TotalProfitCard from "../../widgets/cards/advance/TotalProfitCard.vue";
-import TotalExpenseCard from "../../widgets/cards/advance/TotalExpenseCard.vue";
-import NewEarningCard from "../../widgets/cards/advance/NewEarningCard.vue";
-import NewRevenueColum from "../../widgets/cards/advance/NewRevenueColum.vue";
-export default {
-    // data() {
-    //     const cards = [
-    //         {
-    //             title: "hello",
-    //             count: "23",
-    //             icon:
-    //         }
-    //     ]
-    // },
-
-
-
-    components: {
-        BRow,
-        BCol,
-        UpgradePlanCardOneBg,
-        VisitersCard,
-        OrderVerticalCard,
-        UsersVerticalCard,
-        SubscribeVerticalCard,
-        TicketVerticalCard,
-        BestTeamCard,
-        EarningsCard,
-        ProjectTableCard,
-        ExpensesCard,
-        DownloadCard,
-        RevenueRadarCard,
-        CustomerFeatureCard,
-        OrdersFeatureCard,
-        SaleFeatureCard,
-        ProfitFeatureCard,
-        // new code 
-        VehicleFeatureCard,
-        EarningFeatureCard,
-        TotalProfitCard,
-        TotalExpenseCard,
-        // NewRevenuCard,
-        NewRevenueColum,
-        NewEarningCard,
+  </template>
+  
+  <script>
+  import { BRow, BCol, BCard, BAvatar } from "bootstrap-vue";
+  
+  //  new code
+  import axios from "axios";
+  import NewEarningCard from "../../widgets/cards/advance/NewEarningCard.vue";
+  import NewRevenueColum from "../../widgets/cards/advance/NewRevenueColum.vue";
+  export default {
+      components: {
+      BRow,
+      BCol,
+      BCard,
+      BAvatar,
+      // NewRevenuCard,
+      NewRevenueColum,
+      NewEarningCard,
     },
-};
-</script>
+    data() {
+      return {
+          image: null,
+        isLoading: false,
+        amount: "",
+        driverCount: 0,
+        vehicleCount: 0,
+        b2bCount: 0,
+        b2bDriverCount: 0,
+        b2bVehicleCount: 0,
+        totalEarning: 0,
+        totalProfit: 0,
+        totalExpense: 0,
+      };
+    },
+    mounted() {
+      // Fetch data from your API when the component is mounted
+      axios.get("/dashboard").then((response) => {
+        const data = response.data.data;
+  
+        // Extract data from the API response and set it to your component's data properties
+        this.driverCount = data.driver;
+        this.vehicleCount = data.vehicle;
+        this.b2bCount = data.company;
+        this.b2bDriverCount = data.driverb2b;
+        this.b2bVehicleCount = data.vehicleb2b;
+        this.totalEarning = data.total_earning;
+        this.totalProfit = data.total_profit;
+        this.totalExpense = data.expense;
+      });
+    },
+  
+    
+  };
+  </script>
+  
